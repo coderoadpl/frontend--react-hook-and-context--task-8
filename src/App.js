25fb3bfd367b9d88b2a4fad7431a589c19a945b3
Mateusz Choma <kontakt@coderoad.pl>
2021-09-10 13:55:22 +0200

    Multiple instances

--- src/App.js
+++ src/App.js
@@ -6,7 +6,15 @@ import CounterClass from './CounterClass'
 export const App = () => {
   return (
     <div>
+      <h2>
+        Hooks
+      </h2>
       <Counter />
+      <Counter />
+      <h2>
+        Class
+      </h2>
+      <CounterClass />
       <CounterClass />
     </div>
   )
