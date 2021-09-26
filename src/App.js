34c1a32ce7d2f2594d4ed234ce799c5fba8b3c05
Mateusz Choma <kontakt@coderoad.pl>
2021-09-26 15:21:37 +0200

    `App` modified to allow unmount counters

--- src/App.js
+++ src/App.js
@@ -4,22 +4,41 @@ import Counter from './Counter'
 import CounterClass from './CounterClass'
 
 export const App = () => {
+  const [isVisible, setIsVisible] = React.useState(true)
+
   return (
     <div>
-      <h2>
-        Hooks
-      </h2>
-      <Counter />
-      {/* <Counter
-        startNumber={3}
-      /> */}
-      <h2>
-        Class
-      </h2>
-      <CounterClass />
-      {/* <CounterClass
-        startNumber={3}
-      /> */}
+      <button
+        onClick={() => setIsVisible(!isVisible)}
+      >
+        {
+          isVisible ?
+            'Hide'
+            :
+            'Show'
+        }
+      </button>
+      {
+        isVisible ?
+          <>
+            <h2>
+              Hooks
+            </h2>
+            <Counter />
+            {/* <Counter
+              startNumber={3}
+            /> */}
+            <h2>
+              Class
+            </h2>
+            <CounterClass />
+            {/* <CounterClass
+              startNumber={3}
+            /> */}
+          </>
+          :
+          null
+    }
     </div>
   )
 }
